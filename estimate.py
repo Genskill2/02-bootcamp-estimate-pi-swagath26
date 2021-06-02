676f69f81dd1d8b694ab2ad9640ff267a707a85d
Swagath N S <67425935+swagath26@users.noreply.github.com>
2021-06-02 13:50:13 +0530

    Update estimate.py

--- estimate.py
+++ estimate.py
@@ -1,6 +1,26 @@
 import math
 import unittest
 
+import random
+def wallis(n):
+    sum = 1
+    while(n>0):
+        sum*=(4*n*n)/(4*n*n-1)
+        n-=1
+    return 2*sum
+
+def monte_carlo(n):
+    ctr=0
+    num=n
+    while(n>0):
+        x = random.random()
+        y = random.random()
+        if x*x + y*y < 1:
+            ctr+=1
+        n-=1
+   
+    return 4*(ctr/num)
+
 class TestWallis(unittest.TestCase):
     def test_low_iters(self):
         for i in range(0, 5):
